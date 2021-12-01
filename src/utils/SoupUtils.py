70e528f85fa27f8b91e636f0458c3a6c2c6cadf3
PururunP <pururunp@gmail.com>
2021-12-02 00:27:48 +0900

    SoupUtils追加

---
+++ src/utils/SoupUtils.py
@@ -0,0 +1,20 @@
+'''BeautifulSoupの共通処理'''
+import requests
+from bs4 import BeautifulSoup as bs
+
+
+# URLからHTMLを取得
+def get_soup(url: str):
+    res = requests.get(url)
+    soup = bs(res.content, "html.parser")
+    return soup
+
+
+# タイトルを取得
+def get_title(soup: bs):
+    return soup.title.text
+
+
+# HTMLを整形して全表示
+def print_all_html(soup: bs):
+    print(soup.prettify())
