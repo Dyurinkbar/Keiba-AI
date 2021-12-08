95385edbc50678f9ecd45010b99b8d2236fd5490
PururunP <pururunp@gmail.com>
2021-12-08 12:30:21 +0900

    ファイル名変更

--- src/HorseDataOrganizer.py
+++ src/HorseDataOrganizer.py
@@ -1,9 +1,10 @@
 '''データを整理'''
 import instance.Horse as Horse
+import instance.RaceDetail as RaceDetail
 
 
 # 競走馬の情報からデータを整理
-def horses_data_organize(horses: Horse):
+def horses_data_organize(horses: Horse, race_detail: RaceDetail):
     # 競走馬を順番に情報整理
     for horse in horses:
         print(f'{horse.get_name()}のデータを整理します。')
