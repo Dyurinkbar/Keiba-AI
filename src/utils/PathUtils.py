9cd4c7d70c5da3a445ea66720a064c8ce01b33dd
PururunP <pururunp@gmail.com>
2021-12-09 16:25:35 +0900

    utils調整

--- src/utils/PathUtils.py
+++ src/utils/PathUtils.py
@@ -1,4 +1,6 @@
 '''パスの管理'''
 # 出力先のフォルダ
 global path_out
+global path_textout
 path_out = "./out/"
+path_textout = path_out + "textout/"
--- src/utils/StringUtils.py
+++ src/utils/StringUtils.py
@@ -3,4 +3,9 @@
 
 # 文字列の空白と改行コードをなくして返却
 def replace_not_blank(str: str):
-    return str.replace(" ", "").replace("\n", "")
+    return str.replace(" ", "").replace("　", "").replace("\n", "")
+
+
+# 文字列の空白と丸括弧をなくして返却
+def replace_not_parentheses(str: str):
+    return replace_not_blank(str).replace("(", "").replace(")", "")
